cad3815f52f7873b67ea3afa724e4a168040e9a2
Thujeevan <thujeevan2@gmail.com>
2023-09-17 12:42:22 +0530

    Project initilize

---
+++ expense-track/expense_trackApp.swift
@@ -0,0 +1,17 @@
+//
+//  expense_trackApp.swift
+//  expense-track
+//
+//  Created by Thujeevan on 2023-09-17.
+//
+
+import SwiftUI
+
+@main
+struct expense_trackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
